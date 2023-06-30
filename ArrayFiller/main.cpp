705e8314ace290eaba23fab42d20d4d8a0aa6580
Jo Wagner <jowagner@limago.de>
2023-06-30 09:26:58 +0200

    initial commit

--- ArrayFiller/main.cpp
+++ ArrayFiller/main.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <memory>
 
+
 using namespace std::chrono;
 
 #define MAX_ARRAY_SIZE INT32_MAX/8
@@ -12,22 +13,42 @@ using namespace std::chrono;
 class Demo {
 
     std::shared_ptr<int[]> data;
+
+//    static void foo(int time) {
+//        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+//    }
+
 public:
     auto run() -> void  {
+
+//        std::thread t1{&foo, 1000 };
+//        t1.detach();
+//        std::thread t2{&foo };
+//        std::thread t3{&foo };
+//        std::thread t4{&foo };
+//
+//        if(t1.joinable())
+//            t1.join();
+//        t2.join();
+//        t3.join();
+//        t4.join();
 
         data = std::shared_ptr<int[]>{new int[MAX_ARRAY_SIZE]};
         auto availableProcessors = std::thread::hardware_concurrency();
         std::cout << "Anzahl der Kerne = " << availableProcessors << std::endl;
 
-        auto start = high_resolution_clock::now();
+        for (int i = 1; i <= availableProcessors + 1 ; ++i) {
+            auto start = high_resolution_clock::now();
 
-        for(int i = 0; i < MAX_ARRAY_SIZE; ++ i)
-        {
-            data[i] = rand();
+            for(int i = 0; i < MAX_ARRAY_SIZE; ++ i)
+            {
+                data[i] = rand();
+            }
+
+            auto ende = high_resolution_clock::now();
+            std::cout <<  "Zeit: " << duration_cast<milliseconds>(ende - start).count() << " ms" << std::endl;
+
         }
-
-        auto ende = high_resolution_clock::now();
-        std::cout <<  "Zeit: " << duration_cast<milliseconds>(ende - start).count() << " ms" << std::endl;
 
 
     }
